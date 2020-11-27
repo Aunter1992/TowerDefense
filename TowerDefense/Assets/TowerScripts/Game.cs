de7b27e309bbbc2d215f977e4254c3c9765f475e
wanggeng <1025964889@qq.com>
2020-11-27 17:42:27 +0800

    1127

---
+++ TowerDefense/Assets/TowerScripts/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    Vector2Int _boardSize = new Vector2Int(11, 11);
+
+    [SerializeField]
+    GameBoard board = default;
+
+
+    private void Awake()
+    {
+        board.Initialize(_boardSize);
+    }
+
+
+    private void OnValidate()
+    {
+        if (_boardSize.x < 2)
+            _boardSize.x = 2;
+        if (_boardSize.y < 2)
+            _boardSize.y = 2;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TowerDefense/Assets/TowerScripts/GameBoard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    [SerializeField]
+    Transform _ground;
+
+    [SerializeField]
+    GameTile _tile;
+
+    Vector2Int _size;
+
+    
+    public  void Initialize(Vector2Int size)
+    {
+        this._size = size;
+        _ground.localScale = new Vector3(size.x,1, size.y);
+
+    }
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
